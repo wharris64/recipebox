f44534ab580672602438d9e2352c22cfda71bf8e
nate <nate998877@gmail.com>
2019-11-26 18:38:41 -0500

    implemented edit form and favorite button

--- recipebox/models.py
+++ recipebox/models.py
@@ -3,16 +3,18 @@ from django.contrib.auth.models import User
 
 
 class Author(models.Model):
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length=50)
     biosection = models.TextField()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorites = models.ManyToManyField('Recipe', related_name='fave')
 
     def __str__(self):
-            return self.name
+        return self.name
+
 
 class Recipe(models.Model):
-    title = models.CharField(max_length = 50)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
     description = models.TextField()
     timerequired = models.IntegerField()
     instructions = models.TextField()
